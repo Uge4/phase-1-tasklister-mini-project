296e23e662fcb42d465e798b9cc6d1191fedc8db
eugene <eugeneholenstein@gmail.com>
2023-01-04 22:49:10 +1100

    completed tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let newTaskListItem = document.createElement("li") // new li list item
+  let taskList = document.getElementById("tasks")    // get unordered list object
+  let btn = document.createElement("button") // new 'x' button 
+  btn.textContent = "X"
+  btn.id = "remover"
+  
+
+  document.querySelector("form").addEventListener("submit", (e) => {
+    
+    e.preventDefault()
+    
+
+    // let task = e.target[0].value // get input text 
+    addTask(e.target[0].value)
+    
+    // let newTaskListItem = document.createElement("li") // new li list item
+    // let taskList = document.getElementById("tasks")    // get unordered list object
+    // let btn = document.createElement("button") // new 'x' button 
+    // btn.textContent = "X"
+    
+    // taskList.append(newTaskListItem)
+    // newTaskListItem.textContent = `${task}   `
+    // newTaskListItem.append(btn)
+
+  })
+
+  function addTask(task){
+    taskList.append(newTaskListItem)
+    newTaskListItem.textContent = `${task}   `
+    newTaskListItem.append(btn)
+  }
+
+
+btn.addEventListener('click', e => {
+  btn.parentNode.remove()
+})
+
+
 });
